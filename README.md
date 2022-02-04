74b57cfe4aabef352d9fcba2712fc8a7d1792096
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2022-02-04 11:53:50 +0200

    new

---
+++ README.md
@@ -0,0 +1,5 @@
+# jukbq/jukbq.github.io
+ 
+"# angular-directive-hw2.jukbq.github.io" 
+"# angular-directive-hw2.jukbq.github.io" 
+"# angular-directive-hw2.jukbq.github.io" 
---
+++ src/app/hw2/hw2.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-hw2',
+  templateUrl: './hw2.component.html',
+  styleUrls: ['./hw2.component.scss']
+
+})
+export class Hw2Component {
+  public login = '';
+  public pass = '';
+  public mail = '';
+
+  public loginValid = true;
+
+  public isAdded = true;
+  public userArr: any = [];
+
+
+
+  public editStatus = false;
+  public editIndex!: number;
+
+
+
+  constructor() { }
+
+
+  addUser() {
+
+
+    let user = {
+      login: this.login,
+      pass: this.pass,
+      mail: this.mail
+    };
+    this.userArr.push(user);
+    this.clear();
+  }
+
+  clear() {
+    this.login = ''
+    this.pass = ''
+    this.mail = ''
+  };
+
+  edit(index: number): void {
+    let user = this.userArr[index];
+    this.login = user.login;
+    this.pass = user.pass;
+    this.mail = user.mail;
+    this.editIndex = index;
+    this.editStatus = true;
+  }
+
+  editUser() {
+    let user = this.userArr[this.editIndex];
+    user.login = this.login
+    user.pass = this.pass
+    user.mail = this.mail
+    this.clear();
+    this.editStatus = false;
+  };
+
+  del(index: number): void {
+    this.userArr.splice(index, 1);
+  };
+
+
+};
+
+
